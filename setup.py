bf3f93f9c79c940a3794a411d95ed6675e97750a
QuisEgoSum <subbotin.evdokim@gmail.com>
2025-02-20 18:41:32 +0300

    feat: Refactor trace ID retrieval and dict formatting for extensibility
    Moved trace ID retrieval into a class method to allow overriding. Extracted dict formatting from record into a separate method for easier extension and customization.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='qstd_logger_json_formatter',
-    version='0.1.2',
+    version='0.2.0',
     author='QuisEgoSum',
     author_email='subbotin.evdokim@gmail.com',
     description='Logging json formatter',
